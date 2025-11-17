bc82f82873c12a2ff1395a19227e2fc497345122
neehadj <89594993+neehadj@users.noreply.github.com>
2025-11-17 13:46:00 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_ZV67lueQi', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2kcpfi6mmeide2okhjq58uk8st', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
